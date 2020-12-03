3c08bf58593f9231f636828c91e3ae1808012025
Michael Thomsen <mit@google.com>
2020-12-03 18:07:00 +0100

    Opts

---
+++ implicit_opt_in/script.dart
@@ -0,0 +1,7 @@
+// A small dart script that doesn't really do much.
+void main() {
+  var i = 42;
+  int j;
+
+  print('Hello $i $j');
+}
---
+++ opting_in/bin/opting_in.dart
@@ -0,0 +1,5 @@
+void main() {
+  double d;
+
+  print('Hello ${d.round()}');
+}
